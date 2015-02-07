75d69c928cefcccfff08dd80f5c0cd67f9e0c259
kozw <kozw@firstfloorsoftware.com>
2015-02-07 17:02:57 +0100

    Fixed Xamarin Forms precompile step not being executed Version set to 1.0.5

--- FirstFloor.Xcc/Nuget/ReadMe.txt
+++ FirstFloor.Xcc/Nuget/ReadMe.txt
@@ -2,6 +2,4 @@
 ============================
 Welcome to XCC, a preprocessor adding conditional compilation support to XAML files.
 
-Supporting Windows Universal and Xamarin Forms projects.
-
-See http://xcc.codeplex.com for instructions
+See https://github.com/firstfloorsoftware/xcc for instructions
--- FirstFloor.Xcc/Properties/AssemblyInfo.cs
+++ FirstFloor.Xcc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("First Floor Software")]
 [assembly: AssemblyProduct("Xcc")]
-[assembly: AssemblyCopyright("Copyright © First Floor Software 2014")]
+[assembly: AssemblyCopyright("Copyright © First Floor Software 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
